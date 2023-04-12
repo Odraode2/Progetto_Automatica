535b118769605c6425cf956fbdbb5bf0fa94cdcf
Edoardo Storti <edoardo1.storti@mail.polimi.it>
2023-04-12 19:14:08 +0200

    Nuovi calcoli per trovare valore massimo raggiunto dalla velocità e il primo valore a superare il valore dato. Obiettivo trovare costante di tempo

--- Calcoli_vari.m
+++ Calcoli_vari.m
@@ -1,1 +1,19 @@
-vvggvg
+data = square;
+t = -1;
+position = -1;
+maxv = data(4,1);
+% for i = 1:length(data(4,:))
+%     if data(4,i)>(3.87*(10^4))
+%         t = data(1,i)
+%         position = i
+%         return
+%     end
+% end
+
+for i = 1:length(data(4,:))
+    if data(4,i)>maxv
+        maxv = data(4,i);
+        position = i;
+        t = data(1,i);
+    end
+end
