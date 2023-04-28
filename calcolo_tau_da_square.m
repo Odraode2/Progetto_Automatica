db70fa773aa1d343b4713dcaa9e6131a9236a4bf
Edoardo Storti <edoardo1.storti@mail.polimi.it>
2023-04-28 17:00:52 +0200

    fine lavoro

--- calcolo_tau_da_square.m
+++ calcolo_tau_da_square.m
@@ -1,20 +1,48 @@
 %% Analisi onda quadra
 [tmax, posmax, maxva] = maxval(square);
-[firstTau, posfirst] = findthefirst(square, 0.99*maxva)
+[firstTau, posfirst] = findthefirstfrom(square, 0.99*maxva, 1);
 figure
 plot(square(1,:),square(4,:))
 hold on 
 plot(firstTau,0.99*maxva,'ro','MarkerSize',3)
-primaryTau = firstTau/5
+primaryTau = firstTau/5;
 
-function [x,y] = findthefirst(data, value)
+
+
+%% transitorio di discesa %valori di inizio transitorio
+xin = [];
+FineTransDis=[]; %valori di fine transitorio [tau,indice]
+% trovare il valore i valori di inizio transitorio
+for i = 2:length(square)
+    if square(2,i-1) == 5 && square(2,i) == 0
+        xin(1, size(xin,2)+1) = square(4,i-1);
+        xin(2, size(xin,2)) = i-1;
+    end
+end
+FineTransDis = zeros(2,4);
+for j = size(xin,2)
+    from = xin(2,j);
+    for i = from:size(square,2)
+        if square(4,i) < 0.01*xin(1,j)
+            FineTransDis(1,j) = square(1,i)-square(1,xin(2,j));
+            FineTransDis(2,j) = i;
+            break;
+        end
+    end
+end
+disp(FineTransDis)
+% for i = 1:length(xin(1,:))
+%     [FineTransDis(1,length(FineTransDis)+1),y]=findthefirstfrom(square,0.01*xin(1,i),xin(2,i));
+% end
+%FineTransDis
+function [x,y] = findthefirstfrom(data, value, a)
     t = -1;
     position = -1;
-    for i = 1:length(data(4,:))
-        if data(4,i)>(value)
+    for i = a:length(data(4,:))
+        if data(4,i)>value
             t = data(1,i);
             position = i;
-            x = t;
+            x = t-data(1,a);% t-t_0
             y = position;
             return 
         end
