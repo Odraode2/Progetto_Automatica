d7bee0e851c9139d4c6cd45e0a48307a694d4caa
Edoardo Storti <edoardo1.storti@mail.polimi.it>
2023-05-08 12:36:29 +0200

    ddd

---
+++ fileBrutta1.m
@@ -0,0 +1,20 @@
+clear
+square = importdata("square.mat");
+mi=7722.2;
+t1 = 0.1315;
+t2 = 0.0075;
+T = square(1,500);
+dt = square(1,2)-square(1,1);
+t = 0:dt:T;
+u = 5*ones(1,length(t));
+sys = zpk([],[-t1, -t2], mi);%tf(mi, [1 2 1]);
+opt = stepDataOptions('StepAmplitude',5);
+y = step(sys,t,opt);
+figure
+plot(t,y)
+hold on 
+plot(t,square(4,1:500))
+hold off
+% z=[1 0];
+% p=0.3+0.5j;
+% zplane(z,p)
