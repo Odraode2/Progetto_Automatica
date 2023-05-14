ab5728bade09f10224f2c7afe9795f934ad56f6a
Edoardo Storti <edoardo1.storti@mail.polimi.it>
2023-05-14 09:42:07 +0200

    B

--- bettering_mi.m
+++ bettering_mi.m
@@ -1,8 +1,8 @@
 square = importdata("square.mat");
 mi = 7.696367759707887e+03;
-hp_t1 = 0.13;
+hp_t1 = 0.1315;
 hp_t2 = 0.0075;
-var = 0.01;
+var = 1;
 alpha = -mi*hp_t1/(hp_t2-hp_t1);
 beta = mi*hp_t2/(hp_t2-hp_t1);
 A = [-1/hp_t1,0;0,-1/hp_t2];
@@ -19,22 +19,21 @@ y_data = square(4,1:500).';
 quad_diff = (y_data-y).^2;
 costo1 = sum(quad_diff);
 for i = 1:100000
-    hp_t2x = hp_t2+var;
-    if hp_t2x ~= 0 && hp_t2x ~= hp_t1
-        alpha = -mi*hp_t1/(hp_t2x-hp_t1);
-        beta = mi*hp_t2x/(hp_t2x-hp_t1);
-        A = [-1/hp_t1,0;0,-1/hp_t2x];
-        B = [alpha/hp_t1; beta/hp_t2x];
-        C = [1 1];
-        D = 0;
-    end
+    display(i)
+    mix = mi + var;
+    alpha = -mix*hp_t1/(hp_t2-hp_t1);
+    beta = mix*hp_t2/(hp_t2-hp_t1);
+    A = [-1/hp_t1,0;0,-1/hp_t2];
+    B = [alpha/hp_t1; beta/hp_t2];
+    C = [1 1];
+    D = 0;
     sis = ss(A, B, C, D);
     opt = stepDataOptions('stepAmplitude',5);
     [y,dt,x] = step(sis,square(1,1:500),opt);
     quad_diff = (y_data-y).^2;
     costo2 = sum(quad_diff);
     if costo2 < costo1
-        hp_t2 = hp_t2x;
+        mi = mix;
     else
         var = -(var/10);
     end
@@ -46,19 +45,3 @@ for i = 1:100000
     hold off
 end
  
-    
-%%
-figure
-for i = 0.001:0.001:10*hp_t1
-    hp_t2 = i;
-    alpha = -mi*hp_t1/(hp_t2-hp_t1);
-    beta = mi*hp_t2/(hp_t2-hp_t1);
-    A = [-1/hp_t1,0;0,-1/hp_t2];
-    B = [alpha/hp_t1; beta/hp_t2];
-    C = [1 1];
-    D = 0;
-    sis = ss(A, B, C, D);
-    opt = stepDataOptions('stepAmplitude',5);
-    [y,dt,x] = step(sis,square(1,1:500),opt);
-    
-end
--- fileBrutta1.m
+++ fileBrutta1.m
@@ -7,7 +7,7 @@ T = square(1,500);
 dt = square(1,2)-square(1,1);
 t = 0:dt:T;
 u = 5*ones(1,length(t));
-sys = zpk([],[-t1, -t2], mi);%tf(mi, [1 2 1]);
+sys = zpk([],[-1/t1 -1/t2], mi/(t1*t2));%tf(mi, [1 2 1]);
 opt = stepDataOptions('StepAmplitude',5);
 y = step(sys,t,opt);
 figure
@@ -18,3 +18,5 @@ hold off
 % z=[1 0];
 % p=0.3+0.5j;
 % zplane(z,p)
+y_data = square(4,1:500);
+y_data = y_data'
